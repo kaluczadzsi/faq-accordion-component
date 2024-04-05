a1ccf18419a6e4a1b7a3846a1d02ef068a4981c2
Kalucza Gábor <kaluczadzsi@gmail.com>
2024-04-05 16:53:59 +0200

    max width changed

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <div className="w-full relative">
       <Header />
-      <div className="faq-container absolute top-[50%] left-[50%] translate-x-[-50%] bg-white p-5 w-[80%] rounded-lg max-w-[400px]">
+      <div className="faq-container absolute top-[50%] left-[50%] translate-x-[-50%] bg-white p-5 w-[80%] rounded-lg max-w-[600px]">
         <FAQHeader />
         <FAQList data={FAQ_DATA} />
       </div>
